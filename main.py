1cc3c33b7558d92c95ea74b9de5b2d33c6851ecf
Anton Yushkov <yuschkof.toscha@yandex.ru>
2022-11-10 12:06:16 +0400

    fix

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 
 def vigenere(
-        text: str, 
-        key: str, 
+        text: str,
+        key: str,
         alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя,."!@#$%^&*()_-+= ',
         encrypt=True
 ):
@@ -31,20 +31,22 @@ def vigenere_decrypt(text, key):
     return vigenere(text=text, key=key, encrypt=False)
 
 
-st.header('Шифр Виженера')
+st.title('Шифр Виженера')
 
 col1, col2 = st.columns(2)
 
 with col1:
+    st.subheader('Зашифровать слово')
     output = ''
     word = st.text_input('Слово', 'Hello world')
     key = st.text_input('Ключ', 'key')
-    if st.button('Защифровать'):
+    if st.button('Зашифровать'):
         output = vigenere_encrypt(word, key)
     st.markdown(f"Зашифрованное слово: {output}")
 
 
 with col2:
+    st.subheader('Расшифровать слово')
     output = ''
     shifre = st.text_input('Зашифрованное слово', 'Hello world')
     key = st.text_input('Ключ', '')
