6e3ff139196280008aae4d2fe652620895891b92
Anton Yushkov <yuschkof.toscha@yandex.ru>
2022-11-16 17:02:11 +0400

    Fix

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from vigenere import Vigenere
 
+alphabets = '0123456789' \
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
+            'abcdefghijklmnopqrstuvwxyz' \
+            'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' \
+            'абвгдеёжзийклмнопрстуфхцчшщъыьэюя '
 
 st.title('Шифр Виженера')
 
@@ -12,8 +17,8 @@ with col1:
     word = st.text_input('Слово', 'Hello world')
     keys = st.text_input('Ключ', 'Key')
     if st.button('Зашифровать'):
-        abc = Vigenere(word, keys)
-        output = abc.vigenere_encrypt()
+        abc = Vigenere(word, keys, True)
+        output = abc.algoritm(alphabets)
     st.markdown(f"Зашифрованное слово: {output}")
 
 
@@ -23,6 +28,6 @@ with col2:
     shifre = st.text_input('Зашифрованное слово', '')
     keys = st.text_input('Ключ', '')
     if st.button('Расшифровать'):
-        abc = Vigenere(shifre, keys)
-        output = abc.vigenere_decrypt()
+        abc = Vigenere(shifre, keys, False)
+        output = abc.algoritm(alphabets)
     st.markdown(f"Расшифрованное слово: {output}")
--- vigenere.py
+++ vigenere.py
@@ -1,39 +1,19 @@
 class Vigenere:
 
-    def __init__(self, text, key):
+    def __init__(self, text, key, encrypt):
         self.text = text
         self.key = key
+        self.encrypt = encrypt
 
-    def vigenere_encrypt(self):
-        return vigenere(text=self.text, key=self.key, alphabet=alphabets, encrypt=True)
-
-    def vigenere_decrypt(self):
-        return vigenere(text=self.text, key=self.key, alphabet=alphabets, encrypt=False)
-
-
-alphabets = '0123456789' \
-            'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
-            'abcdefghijklmnopqrstuvwxyz' \
-            'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' \
-            'абвгдеёжзийклмнопрстуфхцчшщъыьэюя '
-
-alphabets_light = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
-                'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-
-
-def vigenere(text, key, alphabet, encrypt=True):
-    result = ''
-    for i in range(len(text)):
-        letter_n = alphabet.index(text[i])
-        key_n = alphabet.index(key[i % len(key)])
-
-        if encrypt:
-            value = (letter_n + key_n) % len(alphabet)
-        else:
-            value = ((letter_n - key_n) - len(alphabet)) % len(alphabet)
-
-        result += alphabet[value]
-
-    return result
-
+    def algoritm(self, alphabet):
+        result = ''
+        for i in range(len(self.text)):
+            letter_n = alphabet.index(self.text[i])
+            key_n = alphabet.index(self.key[i % len(self.key)])
+            if self.encrypt:
+                value = (letter_n + key_n) % len(alphabet)
+            else:
+                value = ((letter_n - key_n) - len(alphabet)) % len(alphabet)
+            result += alphabet[value]
+        return result
 
